1b9e25a6278a23860b5edcfe548837789bd4996e
joaodaniel1604 <143419902+joaodaniel1604@users.noreply.github.com>
2023-09-11 11:12:21 -0300

    Add files via upload

---
+++ atvd6(parte2)/script.js
@@ -0,0 +1,10 @@
+function verificar() {
+    var login = document.getElementById('login')
+    var password = document.getElementById('password')
+    if (login.value != null) {
+        alert(login.value)
+    }
+    if (password.value != null) {
+        alert(password.value)
+    }
+}
